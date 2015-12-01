2416c9bdd45fcf3c95a3acc98393748480beec13
Peter Gyory <ptg3662@rit.edu>
2015-12-01 09:54:58 -0500

    Add eslint and lint task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
-var gulp = require('gulp');
+// Get those sweet gulp libraries in here
+var gulp   = require('gulp'),
+    eslint = require('gulp-eslint');
 
-gulp.task('default', function() {
+gulp.task('lint', function() {
+  return gulp.src(['src/**/*.js','!node_modules/**'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('default', ['lint'], function() {
   console.log('test');
 });
